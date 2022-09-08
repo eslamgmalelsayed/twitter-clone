5817d333969282009b40ca8a8a86d66e57cff8f5
eslamgmalelsayed <eslamgmal1@gmail.com>
2022-09-08 11:47:38 +0300

    setup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+// import 'jquery/src/jquery.js'
+// import 'bootstrap/dist/js/bootstrap.min.js'
 Vue.config.productionTip = false
 
 new Vue({
